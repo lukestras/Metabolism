738a232421e996b763dd077412117179bc802347
matthew <matthew.strasiotto@gmail.com>
2018-07-05 17:02:13 +1000

    Reads whole workbook, next will determine timestamps

--- Data_Process.py
+++ Data_Process.py
@@ -13,8 +13,7 @@ input_excel = 'metabolism_imports.xlsx'
 
 test_in = input_dir + '/' + input_excel
 
-test_ws = 'New_1'
-
+test_ws = None
 """
 Number of rows to be dropped to reach data
 Note that this may change when working with raw txt, and also calibration data 
@@ -27,3 +26,8 @@ calibration_len   = 4
 
 test_cal  = pd.read_excel(test_in, sheet_name=test_ws, skiprows=calibration_start, nrows=calibration_len)
 test_data = pd.read_excel(test_in, sheet_name=test_ws, skiprows=calibration_rows)
+
+#infer timestamps by getting timestamps - min(timestamps)
+
+#for name, data in test_data:
+#    data[]
