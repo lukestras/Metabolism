a3e551a5fbdd35524ea91af6a7d10fc4a6e93459
matthew <matthew.strasiotto@gmail.com>
2018-07-05 16:38:10 +1000

    Starting some test work just using excel book

--- Data_Process.py
+++ Data_Process.py
@@ -2,6 +2,25 @@
 """
 Created on Thu Jul  5 15:41:13 2018
 
-@author: Matthew
+@author: Matthew Strasiotto
 """
+import pandas as pd
 
+
+input_dir = 'IN'
+text_dir  = 'TXT'
+input_excel = 'metabolism_imports.xlsx'
+
+test_in = input_dir + '/' + input_excel
+
+test_ws = 'New_1'
+
+"""
+Number of rows to be dropped to reach data
+Note that this may change when working with raw txt, and also calibration data 
+is PROBABLY of interest to analysis, so consider storing these headers 
+someplace good
+"""
+calibration_rows = 13 
+
+test_df = pd.read_excel()
