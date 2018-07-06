48a7a5088946b29596c947a39130f9d97c2009d1
matthew <matthew.strasiotto@gmail.com>
2018-07-06 18:41:37 +1000

    Separating Source Into Modules
    For Less spaghetti-ish code, better namespace conventions

--- Data_Process.py
+++ Data_Process.py
@@ -9,7 +9,8 @@ import re
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.collections as collections
-
+import namespace as nm
+import work
 
 input_dir = 'IN'
 text_dir  = 'TXT'
@@ -21,43 +22,19 @@ test_ws = None
 out_dir = 'OUT'
 plot_dir = out_dir + '/' + 'Plots'
 
-ts_code_col = 'Time stamp code'
-ts_formatted_col = 'Time (s)'
 
-drop_col_words = [r'temp', r'phase']
 
-"""
-Number of rows to be dropped to reach data
-Note that this may change when working with raw txt, and also calibration data 
-is PROBABLY of interest to analysis, so consider storing these headers 
-someplace good
-"""
-calibration_rows = 13
-calibration_start = 6
-calibration_len   = 4 
+ 
 
-test_cal  = pd.read_excel(test_in, sheet_name=test_ws, skiprows=calibration_start, nrows=calibration_len)
-test_data = pd.read_excel(test_in, sheet_name=test_ws, skiprows=calibration_rows)
+test_cal  = pd.read_excel(test_in, sheet_name=test_ws, skiprows=nm.calibration_start, nrows=nm.calibration_len)
+test_data = pd.read_excel(test_in, sheet_name=test_ws, skiprows=nm.calibration_rows)
 
 """
 Process your data so it's actually good
 """
 for name, data in test_data.items():
-    """
-    infer timestamps by getting timestamps - min(timestamps)
-    """
-    data[ts_formatted_col] = data.loc[:,ts_code_col] - data.loc[:,ts_code_col].min()
-    """
-    Drop columns that aren't O2 related
-    """
-    for word in drop_col_words:
-        data = data[data.columns.drop(list(data.filter(regex=word)))]
     
-    for col in data.columns:
-        if data[col].isnull().all():
-            data = data.drop(columns=col)
-    
-    test_data[name] = data
+    test_data[name] = work.clean_data(data, test_cal[name])
 
 """
 Now make charts, find gradients
@@ -72,20 +49,24 @@ for name, data in test_data.items():
         channel=re.search(r'(CH\s*\d+)',col)
         channel= channel[0]
         color = next(ax._get_lines.prop_cycler)['color']
-        ax.plot(ts_formatted_col,col, data=data, label=channel,color=color)
+        ax.plot(nm.ts_formatted_col,col, data=data, label=channel,color=color)
         #find @ 90% of max
         print(channel)
         threshold_val = (
                 data[col].iloc[0] - 0.1*(data[col].iloc[0]-data[col].min())) 
-        #now slice from %90
+        """
+        now slice from %90
+        %90 is a (semi) arbitrary value
+        """
+        
         lin_df = data.iloc[data[data[col] < threshold_val].index[0]:]
-#        ax.plot(lin_df[ts_formatted_col].iloc[0],lin_df[col].iloc[0],
+#        ax.plot(lin_df[nm.ts_formatted_col].iloc[0],lin_df[col].iloc[0],
 #                color=color,marker='o',linestyle='',
 #                label= channel + 'linear')
-        lin_reg = np.polyfit(lin_df[ts_formatted_col],lin_df[col],1)
+        lin_reg = np.polyfit(lin_df[nm.ts_formatted_col],lin_df[col],1)
         lin_f = np.poly1d(lin_reg)
         lin_eq = '={0:0.3f}x + {1:0.0f}'.format(lin_reg[0],lin_reg[1])
-        ax.plot(data[ts_formatted_col], lin_f(data[ts_formatted_col]), 
+        ax.plot(data[nm.ts_formatted_col], lin_f(data[nm.ts_formatted_col]), 
                 linestyle='-.', color= color, label =channel + ' r' + lin_eq)
         print(channel + ' ',lin_df[col].iloc[0])
         print(channel + ' reg' + lin_eq)
@@ -93,7 +74,7 @@ for name, data in test_data.items():
     
     ax.set_title(name)
     ax.legend()
-    plt.xlabel(ts_formatted_col)
+    plt.xlabel(nm.ts_formatted_col)
     plt.ylabel('O2 [%Air Saturation]')
     plt.savefig(plot_dir + '/' + name + '.png', bbox_inches='tight',pad_inches=0,transparent=True)
     plt.show()
