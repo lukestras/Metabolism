d766ee5558f214ba5a53d813b3a7561858eb5878
matthew <matthew.strasiotto@gmail.com>
2018-08-04 20:06:58 +1000

    Revising IO operations to make them a bit more portable
    Also adding .txt IO operations which should be compat with excel ones, for really fast processing

--- IO.py
+++ IO.py
@@ -15,3 +15,39 @@ def write_out(data_dict, cal_dict, path):
         cal_dict[name].to_excel(writer, name+nm.k_cal)
     writer.save()
     return
+
+
+"""
+Read a single excel workbook, and return a dict of 2 dicts, CAL and DATA 
+each entry in CAL corresponds to the calibration header of the sheet, 
+and each entry in DATA corresponds to the data body of the named sheet.
+"""
+def read_excel(workbook_name):
+    cal_tables = pd.read_excel(test_in, sheet_name=test_ws, skiprows=nm.calibration_start, nrows=nm.calibration_len)
+    data_tables = pd.read_excel(test_in, sheet_name=test_ws, skiprows=nm.calibration_rows)
+    out = {}
+    out[nm.k_cal] = cal_tables
+    out[nm.k_data] = data_tables
+    
+    return out
+
+"""
+Read a single text file, and return a dict of CAL, DATA
+Will need to unpack the output of this a little to put it in a TYPE->Name hierachy
+"""
+def read_text(file_name):
+    separator = r'\t'
+    out = {}
+    out[nm.k_cal] = pd.read_csv(file_name, sep=separator, skiprows=nm.calibration_start, nrows=nm.calibration_len)
+    out[nm.k_data]= pd.read_csv(file_name, sep=separator, skiprows=nm.calibration_rows)
+    return out
+
+"""
+Read everything in a folder into a dict of dicts of dframes
+the hierachy is CAL/DATA->sheet_name
+"""
+def read_text_folder(folder_name, files_to_read=None):
+    if not files_to_read:
+        print("Uh oh")
+        # placeholder, actually do everything unless a list is specified
+    return
